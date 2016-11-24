6ada044aac5409bbfb2319ac3752d248b6d68d7a
Emily Zhai <ezhai24@gmail.com>
2016-11-24 03:20:03 -0800

    starter code and some login

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDGidya0vcwPoNBYRWKbWgFJwpahj21kwU",
+    authDomain: "kearstin-dessert-app.firebaseapp.com",
+    databaseURL: "https://kearstin-dessert-app.firebaseio.com",
+    storageBucket: "kearstin-dessert-app.appspot.com",
+    messagingSenderId: "8921084806"
+};
+firebase.initializeApp(config);
